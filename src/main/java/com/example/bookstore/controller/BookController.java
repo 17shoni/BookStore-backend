712751bd4122c78adeea49cdf8a54c20c8c3ccca
victor_mutua <137988566+17shoni@users.noreply.github.com>
2025-03-24 12:55:09 +0300

    BookController.java

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -22,27 +22,32 @@ public class BookController {
     }
 
     @GetMapping("/getAllBooks")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<Page<BookResponseDTO>> getAllBooks(
             @PageableDefault(size = 10, sort = "title", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.ok(bookService.getAllBooks(pageable));
     }
 
     @GetMapping("getBookById/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<BookResponseDTO> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBookById(id));
     }
 
     @PostMapping("/addBook")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<BookResponseDTO> addBook(@Valid @RequestBody BookRequestDTO bookRequestDTO) {
         return ResponseEntity.ok(bookService.addBook(bookRequestDTO));
     }
 
     @PutMapping("/updateBook/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<BookResponseDTO> updateBook(@PathVariable Long id, @Valid @RequestBody BookRequestDTO bookRequestDTO) {
         return ResponseEntity.ok(bookService.updateBook(id, bookRequestDTO));
     }
 
     @DeleteMapping("/deleteBook/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
