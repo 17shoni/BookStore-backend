f1e0e1763ea2db874fe007f5b2be55f6b1221dd5
victor_mutua <137988566+17shoni@users.noreply.github.com>
2025-03-24 13:06:04 +0300

    SecurityConfig.java

--- src/main/java/com/example/bookstore/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
